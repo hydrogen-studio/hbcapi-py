3c5e2f720c3cc27c1cf2ec27118f9001e7b5ad0f
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-08-15 16:52:38 -0700

    0.2.1

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open("README.rst", "r") as fh:
 setup(
   name = 'hbcapi',         # How you named your package folder (MyLib)
   packages = ['hbcapi'],   # Chose the same as "name"
-  version = '0.2.0',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'The official hydrogenbotsclub api wrapper',   # Give a short description about your library
   author = 'hydrogenbotsclub',                   # Type in your name
   author_email = 'hydrogen.studio.llc@gmail.com',      # Type in your E-Mail
   url = 'https://api.hydrogenbots.club',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/hydrogen-studio/hbcapi-py/archive/0.2.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/hydrogen-studio/hbcapi-py/archive/0.2.1.tar.gz',    # I explain this later on
   keywords = ['hydrogenbotsclub', 'discord', 'club', 'hydrogen', 'hydrogenstudio'],   # Keywords that define your package best
   long_description=long_description,
   install_requires=[            # I get to this in a second
-          'requests'
+          'requests', 'json'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
